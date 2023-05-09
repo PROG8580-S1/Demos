87a464a35cab4e83279f2d83f107d2cefe12c513
Tony Theodoropoulos <theodot@outlook.com>
2023-05-09 11:16:22 -0400

    Fixed indent

--- Week1Demos/ComputeExpressions.java
+++ Week1Demos/ComputeExpressions.java
@@ -4,9 +4,9 @@ public class ComputeExpressions {
 	public static void main(String[] args) {
 		System.out.println("3.5 * 4 / 2 - 2.5 = ");
 		System.out.println(3.5 * 4 / 2 - 2.5);
-		
-		 System.out.print("35 C in F is ");
-		 System.out.println((9.0 / 5) * 35 + 32);
+
+		System.out.print("35 C in F is ");
+		System.out.println((9.0 / 5) * 35 + 32);
 
 	}
 
